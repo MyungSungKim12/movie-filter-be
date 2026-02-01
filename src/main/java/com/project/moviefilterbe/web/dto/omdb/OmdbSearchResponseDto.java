e6b12a0bc3762195bdac6d1f9e2686c583dbbbc2
rlawo <rlawo32@naver.com>
2026-02-01 23:54:52 +0900

    [MovieFilter][BE] Omdb_테스트_코드_20260201

---
+++ src/main/java/com/project/moviefilterbe/web/dto/omdb/OmdbSearchResponseDto.java
@@ -0,0 +1,22 @@
+package com.project.moviefilterbe.web.dto.omdb;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OmdbSearchResponseDto {
+    @JsonProperty("Search")
+    private List<OmdbSearchListDto> search;
+    @JsonProperty("totalResults")
+    private String totalResults;
+    @JsonProperty("Response")
+    private String response;
+    @JsonProperty("Error")
+    private String error;
+}
