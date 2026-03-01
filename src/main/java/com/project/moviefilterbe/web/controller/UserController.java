5a660e0efabf666e3ec840d9c192e91027263aca
rlawo <rlawo32@naver.com>
2026-03-01 23:24:48 +0900

    [MovieFilter][BE] Wishlist_연동_작업_20260301

---
+++ src/main/java/com/project/moviefilterbe/web/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.project.moviefilterbe.web.controller;
+
+import com.project.moviefilterbe.service.UserService;
+import com.project.moviefilterbe.service.api.NaverApiService;
+import com.project.moviefilterbe.service.api.TmdbApiService;
+import com.project.moviefilterbe.service.api.YoutubeApiService;
+import com.project.moviefilterbe.web.dto.TestRequestDto;
+import com.project.moviefilterbe.web.dto.detail.NaverReviewResponseDTO;
+import com.project.moviefilterbe.web.dto.detail.YoutubeVideoResponseDTO;
+import com.project.moviefilterbe.web.dto.movie.MovieListResponseDto;
+import com.project.moviefilterbe.web.dto.user.WishlistRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/wishlist")
+    public void saveWishlist(@RequestBody WishlistRequestDto wishlistRequestDto) {
+        userService.updateWishlist(wishlistRequestDto);
+    }
+}
