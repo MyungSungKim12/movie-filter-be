63b5effeb0fe0fb9be7c931c9a5c77369bb92734
rlawo <rlawo32@naver.com>
2026-03-15 13:48:57 +0900

    [MovieFilter][BE] RestTemplate_전역_처리_20260315

--- src/main/java/com/project/moviefilterbe/config/RestTemplateConfig.java
+++ src/main/java/com/project/moviefilterbe/config/RestTemplateConfig.java
@@ -1,14 +1,20 @@
 package com.project.moviefilterbe.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @Configuration
 public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .connectTimeout(Duration.ofSeconds(10)) // 연결 타임아웃
+                .readTimeout(Duration.ofSeconds(60))    // 읽기 타임아웃
+                .build();
     }
 }
