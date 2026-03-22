368302646f09c82e25baeffe69fc20fff1790525
rlawo <rlawo32@naver.com>
2026-03-22 13:10:09 +0900

    [MovieFilter][BE] 로그인_작업(OAuth2로직)_20260322

--- src/main/java/com/project/moviefilterbe/config/SecurityConfig.java
+++ src/main/java/com/project/moviefilterbe/config/SecurityConfig.java
@@ -1,7 +1,11 @@
 package com.project.moviefilterbe.config;
 
-import com.project.moviefilterbe.login.handler.OAuth2SuccessHandler; // 추가
-import com.project.moviefilterbe.login.service.CustomOAuth2UserService;
+import com.project.moviefilterbe.login.oauth.OAuth2SuccessHandler; // 추가
+import com.project.moviefilterbe.login.jwt.JwtAccessDeniedHandler;
+import com.project.moviefilterbe.login.jwt.JwtAuthenticationEntryPoint;
+import com.project.moviefilterbe.login.jwt.JwtFilter;
+import com.project.moviefilterbe.login.jwt.JwtTokenProvider;
+import com.project.moviefilterbe.login.oauth.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +13,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy; // 추가
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -17,17 +22,23 @@ import org.springframework.web.filter.CorsFilter;
 public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
-    private final OAuth2SuccessHandler oAuth2SuccessHandler; // 추가
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
     private final CorsFilter corsFilter;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtTokenProvider tokenProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .addFilter(corsFilter) // CORS 처리
-                // 1. JWT 사용을 위해 세션 설정을 STATELESS로 변경
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                        .accessDeniedHandler(jwtAccessDeniedHandler)
                 )
                 .formLogin(form -> form.disable())
                 .httpBasic(basic -> basic.disable())
@@ -44,7 +55,8 @@ public class SecurityConfig {
                         )
                         // 2. defaultSuccessUrl 대신 핸들러를 등록
                         .successHandler(oAuth2SuccessHandler)
-                );
+                )
+                .addFilterBefore(new JwtFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
