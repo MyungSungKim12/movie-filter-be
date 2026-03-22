963efb11b0096cced8ff6564741d362eebc796d0
rlawo <rlawo32@naver.com>
2026-03-22 13:11:08 +0900

    [MovieFilter][BE] 로그인_작업(JPA정리)_20260322

--- src/main/java/com/project/moviefilterbe/domain/repository/UserRepository.java
+++ src/main/java/com/project/moviefilterbe/domain/repository/UserRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
-    // 이메일로 기존 가입자인지 확인하기 위한 메서드
-    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUiSocialId(String email);
 }
